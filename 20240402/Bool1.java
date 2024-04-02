718b973599c94387ee367c40d7d7dc630b7f924b
kaje033 <lyusungyun@icloud.com>
2024-04-02 11:39:44 +0900

    Feat: 20240402

---
+++ 20240402/Bool1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Bool1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+
+        System.out.println(num1 == num2);
+    }
+}
